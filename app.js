3c41f474ee02ffe4677b4116560e2fb44b5f9597
RIPUNJAY287 <kripunjay81@gmail.com>
2023-05-13 04:31:41 +0530

    added vercel

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
+
 app.use("/api/merchandise", merchandise);
 app.use("/api/user", user);
 app.use("/api/payment", payment);
